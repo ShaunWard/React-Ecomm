4e597dd3f9d62785d4883b9df74be1013c0b14aa
Shaun Ward <shaunalwynward@googlemail.com>
2021-08-18 22:20:45 +0100

    Started to build app.js with layout styles

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Homepage from './homepage.component';
+import './homepage.styles.scss'
 
 function App() {
   return (
     <div>
-      Hello World
+      <Homepage />
     </div>
   );
 }
