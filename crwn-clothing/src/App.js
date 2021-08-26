0ed81833f24c65b2bdd67ad2e1c1b3c2ec23b222
Shaun Ward <shaunalwynward@googlemail.com>
2021-08-26 13:12:22 +0100

    Built sign up page with sign up and form components

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 import './pages/homepage/homepage.styles.scss'
 
@@ -19,6 +20,9 @@ function App() {
           </Route>
           <Route path='/shop'>
             <ShopPage />
+          </Route>
+          <Route path='/signin'>
+            <SignInAndSignUpPage />
           </Route>
         </Switch>
       </BrowserRouter>
